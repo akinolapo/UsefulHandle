aa4771bdd8f27077f37fdfd8146b61974537b074
Peace Akinola <akinolapo@gmail.com>
2022-04-10 15:06:38 +0100

    added js file for 100 level 2nd semester

---
+++ js/100LevelSecondSemester.js
@@ -0,0 +1,115 @@
+const courseCode = document.querySelectorAll("input[type=checkbox]")
+const grade = document.getElementsByClassName("grade")
+const courseGrade = document.querySelectorAll("input[type=radio]")
+const gradeName = document.querySelectorAll("input[type = radio]")
+const submitValues = document.getElementById("submit_values")
+
+const label = document.getElementsByTagName("label")
+
+let selctedCourse = ""
+
+
+// const csc101 = 2
+// const chm101 = 4
+// const chm103 = 1
+// const tpd101 = 1
+// const mth101 = 5
+// const phy101 = 4
+// const phy107 = 1
+
+
+let courseUnit = 0
+let totalGradePoint = 0
+let gradePoint = 0
+let coursePoint = 0
+
+
+// Check if course code  is selected and display the corsponding radio input to select grade in course 
+
+for (let i = 0; i < courseCode.length; i++) {
+    courseCode[i].addEventListener("click", event => {
+        if (event.target.checked) {
+            if (courseCode[i].value === "csc102") {
+                courseUnit = 2
+                grade[i].style.display = "block"
+            }
+            else if(courseCode[i].value === "chm102"){
+                courseUnit = 4
+                grade[i].style.display ="block"
+            }
+            else if(courseCode[i].value === "chm104"){
+                courseUnit = 1
+                grade[i].style.display ="block"
+            }
+            else if(courseCode[i].value === "mth102"){
+                courseUnit = 1
+                grade[i].style.display ="block"
+            }
+            else if(courseCode[i].value === "mth104"){
+                courseUnit = 5
+                grade[i].style.display ="block"
+            }
+            else if(courseCode[i].value === "phy102"){
+                courseUnit = 4
+                grade[i].style.display ="block"
+            }
+            else if(courseCode[i].value === "phy108"){
+                courseUnit = 1
+                grade[i].style.display ="block"
+            }
+            selctedCourse = courseCode[i].value
+        } else {
+            grade[i].style.display = "none"
+        } return courseUnit
+    })
+}
+
+
+
+// console.log(gradeName[0])
+
+//check when a grade is selcted and run the fuction to get the grade point
+for (let j = 0; j < gradeName.length; j++){
+    // console.log(gradeName[j].value)
+    gradeName[j].addEventListener("click", event => {
+        if (event.target.checked){
+            gradePointCalulation(gradeName[j].value)
+            coursePoint = gradePoint * courseUnit
+            totalGradePoint = coursePoint
+        }    
+    })
+
+}
+
+
+// Check the grade selected and assign grade point
+
+function gradePointCalulation(gradePicked){
+
+    if( gradePicked === "A"){
+        gradePoint = 5
+    } else if (gradePicked === "B"){
+        gradePoint = 4
+    } else if (gradePicked === "C"){
+        gradePoint = 3
+    } else if (gradePicked === "D"){
+        gradePoint = 2
+    } else if (gradePicked === "E"){
+        gradePoint = 1
+    } else{
+        gradePoint = 0
+    }
+    return gradePoint
+}
+
+
+
+
+submitValues.addEventListener("click", function(){
+   
+    console.log(totalGradePoint)
+})
+
+
+
+
--- js/gypee.js
+++ js/gypee.js
@@ -2,13 +2,19 @@ const submitValues = document.getElementById("submit_values")
 const inputValues = document.getElementsByTagName("input")
 const selectedOptions = document.getElementById("result")
 
+let levelSemArrray = []
+
 submitValues.addEventListener("click", function(){
     for(i = 0; i < inputValues.length; i++){
         if(inputValues[i].type = "radio"){
             if(inputValues[i].checked){
-                // selectedOptions.style.display="block"
-                // selectedOptions.innerHTML += `<strong>${inputValues[i].name}</strong>: ${inputValues[i].value} <br>`
-                location.href="/courses.html"
+                levelSemArrray.push(inputValues[i].value)
+                if (levelSemArrray[0] === "100" && levelSemArrray[1] === "first"){
+                    location.href="/100LevelFirstSemester.html"
+                } else if (levelSemArrray[0] === "100" && levelSemArrray[1] === "second"){
+                    location.href="/100LevelSecondSemester.html"
+                }
+                
              }
          }
      }
